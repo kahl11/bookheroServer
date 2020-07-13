1912f266efe613c358d08592b3dac5b27efb7a22
kevin ahl <kahl11@yahoo.ca>
2020-07-13 18:22:59 +0000

    registration and login is working

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import secrets
 import hashlib
 import time
 import urllib.parse
+import json
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -16,31 +17,38 @@ mydb = mysql.connector.connect(
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/createUser')
+@app.route('/createUser', methods=['POST'])
 def index():
+    username = request.json.get("username")
     mycursor = mydb.cursor()
-    sql = "INSERT INTO users (username, salt, password, token) VALUES (%s, %s, %s, %s)"
+    sql = "SELECT COUNT(*) FROM users WHERE username like %s"
+    mycursor.execute(sql, (username,))
+    myresult = mycursor.fetchall()
+    if(myresult[0][0] >= 1):
+        return json.dumps({"status":"duplicate"})
+    sql = "INSERT INTO users (username, salt, password, token, school, email) VALUES (%s, %s, %s, %s, %s, %s)"
     salt = secrets.token_urlsafe(128)
-    hashedPass = hashlib.sha224(str(request.args.get('password')+salt).encode()).hexdigest()
-    val = (request.args.get('username'), salt, hashedPass, secrets.token_urlsafe(128))
+    hashedPass = hashlib.sha224(str(request.json.get('password')+salt).encode()).hexdigest()
+    val = (username, salt, hashedPass, secrets.token_urlsafe(128), request.json.get("school"), request.json.get("email"))
     mycursor.execute(sql, val)
 
     mydb.commit()
-    return 'Index Page'
+    return json.dumps({"status":"success"})
 
 @app.route('/login', methods=['POST'])
 def login():
-    password = request.values.get("password").replace("%","")
-    username = request.values.get("username").replace("%","")
+    password = request.json.get("password").replace("%","")
+    username = request.json.get("username").replace("%","")
+    print(username)
     if(urllib.parse.quote(password) != password or urllib.parse.quote(username) != username):
-        return "false"
+        return json.dumps({"token":"false"})
 
     mycursor = mydb.cursor()
     sql = ("SELECT * FROM users WHERE username like %s")
     mycursor.execute(sql, (username, ))
     myresult = mycursor.fetchall()
     if(len(myresult) < 1):
-        return "false"
+        return json.dumps({"token":"false"})
     salt = myresult[0][1]
     hashedPass = myresult[0][2]
     testPass = hashlib.sha224(str(password+salt).encode()).hexdigest()
@@ -49,10 +57,10 @@ def login():
         sql = "UPDATE users SET token = %s WHERE username like %s"
         mycursor.execute(sql, (token, username))
         mydb.commit()
-        return token
+        return json.dumps({"token":token})
     else:
         time.sleep(0.1)
-        return "false"
-    return "false"
+        return json.dumps({"token":"false"})
+    return json.dumps({"token":"false"})
 
 app.run()
